0c49a292b054c51ab3cda3d46d9462bb5b05ae1f
xiao_zeng <zengxiao1028@gmail.com>
2017-01-02 14:39:39 -0500

    first

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ from keras.preprocessing import sequence
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras.layers import Dense, Dropout, Activation, Embedding
+from keras.layers import Dense, Dropout, Activation, Embedding,Bidirectional
 from keras.layers import LSTM, SimpleRNN, GRU
 from keras.regularizers import l2
 
@@ -39,9 +39,9 @@ def split_sub_sample(x, y,win_len):
     for idx, each in enumerate(x):
 
         original_len = each.shape[0]
-        split_point = original_len*4/5
-        train = each[:split_point]
-        test = each[split_point:]
+        split_point = original_len*1/5
+        train = each[split_point:]
+        test = each[:split_point]
 
         for i in range(0, train.shape[0] - win_len + 1, 1):
             x_win = train[i:i+win_len]
@@ -57,9 +57,8 @@ def split_sub_sample(x, y,win_len):
 def get_model():
     print('Build model...')
     model = Sequential()
-    model.add(LSTM(64, dropout_W=0.5, dropout_U=0.5, input_dim=X_train.shape[2],
-                   input_length=X_train.shape[1], return_sequences=True,
-                   W_regularizer=l2(0.01), U_regularizer=l2(0.01)))
+    model.add(LSTM(64, dropout_W=0.5, dropout_U=0.5,  return_sequences=True,
+                   W_regularizer=l2(0.01), U_regularizer=l2(0.01), input_shape=(X_train.shape[1],X_train.shape[2])))
     model.add(LSTM(64, dropout_W=0.5, dropout_U=0.5, W_regularizer=l2(0.01), U_regularizer=l2(0.01)))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
@@ -77,10 +76,10 @@ if __name__ == '__main__':
     #x = [each[0] for each in x]
     y = np.array(y)
 
-    win_len = 10
+    win_len = 7
     batch_size = 16
 
-    cross_subject = True
+    cross_subject = False
     if cross_subject:
         #X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.1,random_state = 2)
         loo = LeaveOneOut()
@@ -98,9 +97,10 @@ if __name__ == '__main__':
             X_train = np.array(X_train)
             X_test  = np.array(X_test)
 
+
             model = get_model()
             print('Train...')
-            model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=100,
+            model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=50,
                       validation_data=(X_test, y_test))
             score, acc = model.evaluate(X_test, y_test,
                                         batch_size=batch_size)
@@ -115,6 +115,20 @@ if __name__ == '__main__':
         X_train, X_test, y_train, y_test = split_sub_sample(x, y, win_len=win_len)
         X_train = sequence.pad_sequences(X_train, maxlen=win_len)
         X_test = sequence.pad_sequences(X_test, maxlen=win_len)
+        X_train = np.array(X_train)
+        X_test = np.array(X_test)
+
+        model = get_model()
+        print('Train...')
+        model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=50,
+                  validation_data=(X_test, y_test))
+        score, acc = model.evaluate(X_test, y_test,
+                                    batch_size=batch_size)
+        print('')
+        print('Test score:', score)
+        print('Test accuracy:', acc)
+
+        print('accuracy:', acc)
 
 
 
---
+++ run_traditional.py
@@ -0,0 +1,123 @@
+import os
+import pickle
+
+import numpy as np
+
+import datetime,time
+import MyConfig
+from datetime import timedelta
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import LeaveOneOut
+from keras.preprocessing import sequence
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+
+def sub_sample(x, y,win_len):
+    new_x = []
+    new_y = []
+
+    for idx, each in enumerate(x):
+        original_len = each.shape[0]
+        for i in range(0, original_len - win_len + 1, 1):
+            x_win = each[i:i+win_len]
+            new_x.append(x_win)
+            new_y.append(y[idx])
+
+    return new_x,np.array(new_y)
+
+def split_sub_sample(x, y,win_len):
+    X_train = []
+    X_test = []
+    y_train = []
+    y_test = []
+    for idx, each in enumerate(x):
+
+        original_len = each.shape[0]
+        split_point = original_len*4/5
+        train = each[:split_point]
+        test = each[split_point:]
+
+        for i in range(0, train.shape[0] - win_len + 1, 1):
+            x_win = train[i:i+win_len]
+            X_train.append(x_win)
+            y_train.append(y[idx])
+        for i in range(0, test.shape[0] - win_len + 1, 1):
+            x_win = test[i:i+win_len]
+            X_test.append(x_win)
+            y_test.append(y[idx])
+
+    return np.array(X_train),np.array(X_test),np.array(y_train),np.array(y_test)
+
+def get_model():
+    print('Build model...')
+    names = ["Random Forest","AdaBoost",
+             "Naive Bayes", "QDA"]
+    classifiers = [
+        RandomForestClassifier(n_estimators=50),
+        AdaBoostClassifier(),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis()]
+
+    return names,classifiers
+
+
+if __name__ == '__main__':
+
+    x,y = joblib.load('xiao_dataset_act_cal_scr_lig.pkl')
+    #x = [each[0] for each in x]
+    y = np.array(y)
+
+    win_len = 10
+    batch_size = 16
+
+    cross_subject = True
+    if cross_subject:
+        names, classifiers = get_model()
+        for name, classifier in zip(names, classifiers):
+            print('Using'+name + '\n')
+
+            loo = LeaveOneOut()
+            accs = []
+
+            print('Train...')
+            for idx,(train_idx, test_idx) in enumerate(loo.split(x)):
+                #X_train, X_test = x[train_idx], x[test_idx]
+                X_train = [x[i] for i in train_idx]
+                X_test = [x[i] for i in test_idx]
+                y_train, y_test = y[train_idx], y[test_idx]
+
+                X_train,y_train = sub_sample(X_train,y_train,win_len = win_len)
+                X_test, y_test = sub_sample(X_test, y_test,win_len = win_len)
+                X_train = sequence.pad_sequences(X_train, maxlen=win_len)
+                X_test = sequence.pad_sequences(X_test, maxlen=win_len)
+                X_train = np.array(X_train)
+                X_test  = np.array(X_test)
+                X_train = np.reshape(X_train,(X_train.shape[0],-1))
+                X_test = np.reshape(X_test, (X_test.shape[0], -1))
+                classifier.fit(X_train, y_train)
+                acc = classifier.score(X_test, y_test)
+                accs.append(acc)
+            print('Final Leave one out accuracy:',np.mean(accs))
+
+    else:
+        X_train, X_test, y_train, y_test = split_sub_sample(x, y, win_len=win_len)
+        X_train = sequence.pad_sequences(X_train, maxlen=win_len)
+        X_test = sequence.pad_sequences(X_test, maxlen=win_len)
+
+
+
+
+
+
+
+
